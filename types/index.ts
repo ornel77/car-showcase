5bbb41a833e7043a0f2aaa2e7f014c8e6ce298ef
ornel77 <t.ornel@yahoo.fr>
2023-09-03 10:15:37 +0200

    fetch and car container ok

--- types/index.ts
+++ types/index.ts
@@ -1,17 +1,35 @@
-import { MouseEventHandler } from "react";
+import { MouseEventHandler } from 'react';
 
 export interface CustomButtonProps {
-    title: string,
-    btnType?: "button" | "submit",
-    containerStyles?: string,
-    handleClick?: MouseEventHandler<HTMLButtonElement>
+  title: string;
+  btnType?: 'button' | 'submit';
+  containerStyles?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+  textStyles?: string;
+  rightIcon?: string;
+  isDisabled?: boolean;
 }
 
 export interface CustomFilterProps {
-    title: string,
+  title: string;
 }
 
 export interface SearchManufacturerProps {
-    manufacturer: string,
-    setManufacturer: (manufacturer: string) => void
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => void;
+}
+
+export interface CarProps {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
 }
